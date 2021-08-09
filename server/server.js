76c6c08ef35892f61dc498c6ecd9a5412e05e259
hotbagels <81516937+hotbagels@users.noreply.github.com>
2021-08-09 19:22:54 -0400

    Ran production build

--- server/server.js
+++ server/server.js
@@ -22,15 +22,15 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static(path.join(__dirname, '../client/build')));
-// }
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
 
-//  graphQL
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, '../client/build/index.html'));
-// });
-// 
+ graphQL
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
 
 // app.use(routes);
 
